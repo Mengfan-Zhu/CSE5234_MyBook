fb3e9c650c4f23f48639bd968f0d0f207cc51418
lwosu <58704620+lwosu@users.noreply.github.com>
2020-10-14 16:21:01 -0400

    Add files via upload

---
+++ MyBook-InventoryManagement-EJB/ejbModule/edu/osu/cse5234/bussiness/InvertoryServiceBean.java
@@ -0,0 +1,19 @@
+package edu.osu.cse5234.bussiness;
+
+import edu.osu.cse5234.bussiness.view.InvertoryService;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class InvertoryServiceBean
+ */
+@Stateless
+public class InvertoryServiceBean implements InvertoryService {
+
+    /**
+     * Default constructor. 
+     */
+    public InvertoryServiceBean() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
---
+++ MyBook-InventoryManagement-EJBClient/ejbModule/edu/osu/cse5234/bussiness/view/InvertoryService.java
@@ -0,0 +1,8 @@
+package edu.osu.cse5234.bussiness.view;
+
+import javax.ejb.Remote;
+
+@Remote
+public interface InvertoryService {
+
+}
